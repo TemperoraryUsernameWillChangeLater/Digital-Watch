8ae7825b29784bbfcd0c7111b8f8c6bc9f12fe3e
Gabriel <gabrielvisorde@gmail.com>
2026-05-17 17:28:17 +0000

    Refactor stopwatch_control module to improve lap hold functionality and state management

--- rtl/stopwatch_control.sv
+++ rtl/stopwatch_control.sv
@@ -5,28 +5,37 @@ module stopwatch_control (
     input logic rise_start_stop, // Button[0]
     input logic rise_lap, // Button[1]
     output logic counter_rst,
-    output logic counter_enable,
-    output logic lap_hold
+    output logic counter_enable, /*High: The stopwatch is running || Low: stopwatch is stopped */
+    output logic lap_hold /*High: Display is frozen || Low: Display is live*/
 );
-    logic lap_hold_state, lap_hold_next_state;
-    initial rise_lap = 0;
 
-    initial begin
-        counter_rst = 1'b0;
-        counter_enable = 1'b0;
-        lap_hold = 1'b0;
+    logic [2: 0] state = {counter_rst, counter_enable, lap_hold};
+
+    logic [2: 0] next_state;
+
+    always_comb begin
+        if (rise_lap) begin
+            casez (state)
+                3'b?10 : next_state = 3'b011;
+                3'b?11 : next_state = 3'b010;
+                3'b?01 : next_state = 3'b000;
+                3'b?00 : next_state = 3'b100;
+                default: next_state = state;
+            endcase
+        end
     end
 
     always_ff @(posedge clk) begin
-        lap_hold_state <= lap_hold_next_state;
+        if (rise_start_stop) begin
+            casez (state)
+                3'b?00 : state <= 3'b001;
+                3'b?01 : state <= 3'b000;
+                3'b?10 : state <= 3'b101;
+                3'b?11 : state <= 3'b100;
+                default: state <= state;
+            endcase
+        end else begin
+            state <= next_state;
+        end
     end
-
-    always_comb begin
-        if(rise_lap && counter_enable) begin
-            lap_hold_next_state = 0;
-        end else 
-    end
-
-    assign lap_hold = lap_hold_next_state;
-
 endmodule
